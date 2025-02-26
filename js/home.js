3c191dd4e17532074771a38e7ce119fa52546b67
AlexTimoncini <alextimonci@gmail.com>
2025-02-26 20:35:47 +0100

    section about completed

--- js/home.js
+++ js/home.js
@@ -1,4 +1,22 @@
 init()
 function init() {
-    console.log("HOMEPAGE")
+    let activeText = 0,
+        titles = [
+            "Alex<br>Timoncini",
+            "I'm a<br>Web Developer"
+        ]
+    setInterval(()=>{
+        if(activeText){
+            activeText = 0
+        } else {
+            activeText = 1
+        }
+        document.getElementById("multi_title").classList.add("fade-in")
+        setTimeout(()=>{
+            document.getElementById("multi_title").innerHTML = titles[activeText]
+        }, 250)
+        setTimeout(()=>{
+            document.getElementById("multi_title").classList.remove("fade-in") 
+        }, 500)
+    },6000)
 }
