3af56bd63003b0f04aedbc30cb984fa284a2c480
Poratile_Alex <>
2025-02-28 12:13:21 +0100

    skills page init

--- js/skills.js
+++ js/skills.js
@@ -1,22 +1,6 @@
 init()
 function init() {
-    let activeText = 0,
-        titles = [
-            "Alex<br>Timoncini",
-            "I'm a Web<br>Developer"
-        ]
-    setInterval(()=>{
-        if(activeText){
-            activeText = 0
-        } else {
-            activeText = 1
-        }
-        document.getElementById("multi_title").classList.add("fade-in")
-        setTimeout(()=>{
-            document.getElementById("multi_title").innerHTML = titles[activeText]
-        }, 250)
-        setTimeout(()=>{
-            document.getElementById("multi_title").classList.remove("fade-in") 
-        }, 500)
-    },6000)
+    setTimeout(()=>{
+        fadeIn();
+    },550)
 }
