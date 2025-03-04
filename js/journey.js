7fd45133bcec725eaa27dc37dde596721bafe9b0
AlexTimoncini <alextimonci@gmail.com>
2025-03-04 01:49:07 +0100

    new animation function

--- js/journey.js
+++ js/journey.js
@@ -17,24 +17,29 @@ async function init() {
         let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
             let drawLength = pathLength * scrollPercentage;
             path.style.strokeDashoffset = pathLength - drawLength;
-        })
-    */
+        }))*/
+
     let path = document.querySelector("#paperBoat path"),
-        pathLength = path.getTotalLength()
-    path.style.strokeDasharray = pathLength+" "+pathLength
-    path.style.strokeDashoffset = pathLength
-    setTimeout(async function(){
-        for(let i=pathLength;i>0;i--){
-            i -= 30 * (i/pathLength)
-            if(i<0){
-                i=0
+        pathLength = path.getTotalLength();    
+    path.style.strokeDasharray = `${pathLength} ${pathLength}`;
+    path.style.strokeDashoffset = pathLength;
+
+    setTimeout(() => {
+        function animate() {
+            pathLength -= 25+(pathLength / path.getTotalLength());
+            if (pathLength < 0) pathLength = 0;
+            path.style.strokeDashoffset = pathLength;
+
+            if (pathLength > 0) {
+                requestAnimationFrame(animate);
+            } else {
+                document.getElementById("diveIn").classList.add("active");
+                enableScroll();
             }
-            path.style.strokeDashoffset = i
-            await sleep(1)
         }
-        document.getElementById("diveIn").classList.add("active")
-        enableScroll()
-    },100)
+        
+        requestAnimationFrame(animate);
+    }, 100);
 
 }
 function sleep(ms) {
